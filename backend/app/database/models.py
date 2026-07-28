d9728aa11569c8a45035ee78723be611948619bb
BalaKishnann <gbalakishnan@gmail.com>
2026-07-28 04:50:54 -0400

    Add TaskHistory model to database

--- backend/app/database/models.py
+++ backend/app/database/models.py
@@ -1,1 +1,29 @@
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.sql import func
 
+from app.database.database import Base
+
+
+class TaskHistory(Base):
+
+    __tablename__ = "task_history"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    task = Column(String)
+
+    selected_tool = Column(String)
+
+    status = Column(String)
+
+    result = Column(String)
+
+    trace = Column(String)
+
+    created_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
