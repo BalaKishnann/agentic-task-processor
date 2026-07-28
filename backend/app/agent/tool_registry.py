57a086fca6c1bbd402d720dedbc87ad62dcb4715
BalaKishnann <gbalakishnan@gmail.com>
2026-07-28 04:35:40 -0400

    Refactor ToolRegistry to improve execute method

--- backend/app/agent/tool_registry.py
+++ backend/app/agent/tool_registry.py
@@ -2,46 +2,28 @@ from app.tools.calculator_tool import CalculatorTool
 from app.tools.text_tool import TextTool
 from app.tools.weather_tool import WeatherTool
 
-
 class ToolRegistry:
+  
+  def __init__(self):
+    self.tools = [
+        CalculatorTool(),
+        TextTool(),
+        WeatherTool()
+      ]
 
-    def __init__(self):
+   def execute(self, task: str, trace):
+    
+    trace.add("Searching registered tools.")
 
-        self.tools = [
-            CalculatorTool(),
-            TextTool(),
-            WeatherTool()
-        ]
-    """
-    def execute(self, task: str):
+    for tool in self.tools:
 
-        for tool in self.tools:
-
-            if tool.can_handle(task):
-                return tool.execute(task)
-
+      if tool.can_handle(task):
+        trace.add(f"{tool.__class__.__name__} matched the request.")
+        
+        return tool.execute(task, trace)
+        trace.add("No matching tool found.")
         return {
-            "tool": "Unknown",
-            "message": "No suitable tool found."
-        }
-        """
-    
-     def execute(self, task: str, trace):
-    
-            trace.add("Searching registered tools.")
-    
-            for tool in self.tools:
-    
-                if tool.can_handle(task):
-    
-                    trace.add(f"{tool.__class__.__name__} matched the request.")
-    
-                    return tool.execute(task, trace)
-    
-            trace.add("No matching tool found.")
-    
-            return {
-                "status": "FAILED",
-                "message": "No suitable tool found.",
-                "trace": trace.get_steps()
+            "status": "FAILED",
+            "message": "No suitable tool found.",
+            "trace": trace.get_steps()
             }
