92f8d2112c44f019d4eaaf3469064c5e9701db95
BalaKishnann <gbalakishnan@gmail.com>
2026-07-28 03:49:55 -0400

    Implement ToolRegistry to manage tools

--- backend/app/agent/tool_registry.py
+++ backend/app/agent/tool_registry.py
@@ -1,1 +1,26 @@
+from app.tools.calculator_tool import CalculatorTool
+from app.tools.text_tool import TextTool
+from app.tools.weather_tool import WeatherTool
 
+
+class ToolRegistry:
+
+    def __init__(self):
+
+        self.tools = [
+            CalculatorTool(),
+            TextTool(),
+            WeatherTool()
+        ]
+
+    def execute(self, task: str):
+
+        for tool in self.tools:
+
+            if tool.can_handle(task):
+                return tool.execute(task)
+
+        return {
+            "tool": "Unknown",
+            "message": "No suitable tool found."
+        }
